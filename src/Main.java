2493421257c5526a7c31dfdbcc54d4a8890a4842
aliarjomandbigdeli <aliabigdeli@gmail.com>
2018-12-24 17:54:28 +0330

    add state and problem classes add initialState ,goalTest,nextState methods

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
+        Problem p = new Problem();
+        System.out.println(p.stepCost(14, 15));
     }
 }
---
+++ src/State.java
@@ -0,0 +1,11 @@
+public class State {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public State(int id) {
+        this.id = id;
+    }
+}
